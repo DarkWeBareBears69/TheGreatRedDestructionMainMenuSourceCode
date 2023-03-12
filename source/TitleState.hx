81af88bf5cf25c7d1512118061e37583ce338fb7
DarkWeBareBears69 <84250565+DarkWeBareBears69@users.noreply.github.com>
2023-03-12 11:26:36 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -493,11 +493,7 @@ class TitleState extends MusicBeatState
 
 				new FlxTimer().start(1, function(tmr:FlxTimer)
 				{
-					if (mustUpdate) {
-						MusicBeatState.switchState(new OutdatedState());
-					} else {
-						MusicBeatState.switchState(new MainMenuState());
-					}
+					MusicBeatState.switchState(new OutdatedState());
 					closedState = true;
 				});
 				// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
