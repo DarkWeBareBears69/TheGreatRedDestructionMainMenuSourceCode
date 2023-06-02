e6c99aa089d5a2d9087960f4294a5189b83d228d
DarkWeBareBears69 <84250565+DarkWeBareBears69@users.noreply.github.com>
2023-06-02 11:06:00 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import introGame;
 import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
 import flixel.FlxGame;
@@ -33,7 +34,7 @@ class Main extends Sprite
 	var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: TitleState, // initial game state
+		initialState: introGame, // initial game state
 		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
