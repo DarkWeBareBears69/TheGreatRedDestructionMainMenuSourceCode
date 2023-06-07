e36c8aa0f73181719211e35e5154a3e235208618
DarkWeBareBears69 <84250565+DarkWeBareBears69@users.noreply.github.com>
2023-06-07 09:45:07 +0800

    Update OutdatedState.hx

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -24,12 +24,12 @@ class OutdatedState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
-			please update to " + TitleState.updateVersion + "!\n
-			Press ESCAPE to proceed anyway.\n
+			"Hey, this build was made with the customized build of the\n
+			old version of Psych Engine (" + MainMenuState.psychEngineVersion + ")!\n
+			The recent version of Psych is " + TitleState.updateVersion + "!\n
 			\n
-			Thank you for using the Engine!",
+			Press ENTER or ESC to proceed anyway.\n
+			Thank you for downloading the mod!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
@@ -41,7 +41,6 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
