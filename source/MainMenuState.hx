44bb0a01f91c2040fccd01c4f25c2ff937616991
DarkWeBareBears69 <84250565+DarkWeBareBears69@users.noreply.github.com>
2025-05-14 09:57:33 +0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -123,7 +123,7 @@ class MainMenuState extends MusicBeatState
 		}
 
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 84, 0, "FNF: The Great Red Destruction v6.3.0", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 84, 0, "FNF: The Great Red Destruction v6.3.1", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
